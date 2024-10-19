a92d584ae758a44373a96b971d6c415e43e0876f
Lucas Oliveira Silvério <oliveiratricolorfc@hotmail.com>
2024-10-19 19:39:09 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/los/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.los.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
