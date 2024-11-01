d2969b4762dee75685df8d47ed4bc01b927ceb90
Lucas Oliveira Silvério <oliveiratricolorfc@hotmail.com>
2024-10-31 21:05:43 -0300

    Consulta simples com @Query

--- src/main/java/com/los/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/los/workshopmongo/repository/PostRepository.java
@@ -2,12 +2,16 @@ package com.los.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.los.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
